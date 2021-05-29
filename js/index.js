3a74a3da3ce187a7a0f55c7af302d13f60e592b0
Katleho-codes <67188889+Katleho-codes@users.noreply.github.com>
2021-05-29 14:37:21 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// TOGGLE NAVBAR
+// menu
+$(document).ready(function () {
+	$('.toggle').click(function () {
+		$('.menu').toggleClass('active');
+	});
+});
+
+// popup images IN PHOTOGRAPHY SECTION
+$(document).ready(function () {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function (item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+});
+
+// tooltips
+$(document).ready(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+// date for copyright
+var d = new Date();
+document.getElementById("demo").innerHTML = "Copyright " + d.getFullYear();
